f1f509972d0faf527ca5e84ee52f189d5fa138fe
Prasad Vernekar <prasadvernekar72@gmail.com>
2026-06-27 23:52:49 +0530

    feat: add login test scenarios

---
+++ pages/fixture.js
@@ -0,0 +1,17 @@
+import { test as base, expect, request } from '@playwright/test';
+import LoginPage from './LoginPage.js';
+
+
+export const test = base.extend({
+ loginPage: async ({ page }, use) => {
+   await use(new LoginPage(page));
+ },
+
+
+ 
+
+
+});
+
+
+export { expect };
